5b6b415d59c3031346cadeffce50958d02e092f3
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-11-17 12:43:30 -0300

    crop,upload,firebase inicialized

--- Projeto/public/files/app.js
+++ Projeto/public/files/app.js
@@ -1,12 +1,87 @@
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="/__/firebase/8.0.2/firebase-app.js"></script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAfy0Lz3vjVccE3XSBb4rQpI3-1zM2N0SY",
+    authDomain: "imagenator-ccdab.firebaseapp.com",
+    databaseURL: "https://imagenator-ccdab.firebaseio.com",
+    projectId: "imagenator-ccdab",
+    storageBucket: "imagenator-ccdab.appspot.com",
+    messagingSenderId: "571893378531",
+    appId: "1:571893378531:web:4668c69f6606df79f204d6",
+    measurementId: "G-LLGZY6SCGE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+    console.log(firebase);
 
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="/__/firebase/8.0.2/firebase-analytics.js"></script>
+    function uploadImage() {
+      const ref = firebase.storage().ref();
+      const file = document.querySelector("#photo").files[0];
+      const name = +new Date() + "-" + file.name;
+      const metadata = {
+        contentType: file.type
+      };
+      const task = ref.child(name).put(file, metadata);
+      task
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+          console.log(url);
+          document.querySelector("#image").src = url;
+        })
+        .catch(console.error);
+    }
 
-// <!-- Initialize Firebase -->
-// <script src="/__/firebase/init.js"></script>
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById("snap");
+const errorMsgElement = document.querySelector('span#errorMsg');
 
+const constraints = {
+  audio: false,
+  video: {
+    width: 640, height: 480
+  }
+};
 
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
 
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+// Load init
+init();
+
+// Draw image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
+    var image = new Image();
+image.id = "pic";
+image.src = canvas.toDataURL();
+console.log(image.src)
+var button = document.createElement('button')
+
+button.textContent = 'Upload'
+
+button.classList.add('button')
+document.body.appendChild(button)
+
+button.onclick = function() {
+    const ref = firebase.storage().ref();
+    ref.child(new Date() + '-' + 'base64').putString(image.src, 'data_url').then(function(snapshot) {
+    console.log('Uploaded a data_url string!');
+    alert("Image Uploaded")
+});
+}
+});
--- Projeto/public/files/script.js
+++ Projeto/public/files/script.js
@@ -1,183 +1,183 @@
-// // TODO:  SELEÇÃO DA IMGAME PARA VISUALIZAÇÃO
-// let btnSeletorFoto = document.getElementById("btnSeletorDeFotos");
-// let btnBaixar = document.getElementById("btnBaixar");
-// let btnCorteFoto = document.getElementById("btnCorteFoto");
-// let arquivoDaFoto = document.getElementById("arquivoDaFoto");
-// let recebeFoto = document.getElementById("recebeFoto");
-// let nomeFoto;
-// let imagem;
-// //Adicionar evento 'click' no elemento #seletorDeFotos, que irá executar a funcionalidade de clicar no elemento #arquivoDaFoto
-// btnSeletorFoto.onclick = () => {
-//   arquivoDaFoto.click();
-// };
-// //Ao terminar o evento de carregar a DOM
-// window.addEventListener("DOMContentLoaded", () => {
-//   //Adicionar evento 'change' no elemento #arquivoDaFoto
+// TODO:  SELEÇÃO DA IMGAME PARA VISUALIZAÇÃO
+let btnSeletorFoto = document.getElementById("btnSeletorDeFotos");
+let btnBaixar = document.getElementById("btnBaixar");
+let btnCorteFoto = document.getElementById("btnCorteFoto");
+let arquivoDaFoto = document.getElementById("arquivoDaFoto");
+let recebeFoto = document.getElementById("recebeFoto");
+let nomeFoto;
+let imagem;
+//Adicionar evento 'click' no elemento #seletorDeFotos, que irá executar a funcionalidade de clicar no elemento #arquivoDaFoto
+btnSeletorFoto.onclick = () => {
+  arquivoDaFoto.click();
+};
+//Ao terminar o evento de carregar a DOM
+window.addEventListener("DOMContentLoaded", () => {
+  //Adicionar evento 'change' no elemento #arquivoDaFoto
 
-//   arquivoDaFoto.addEventListener("change", (e) => {
-//     // Mostra o botao de download
-//     btnBaixar.style.display = "none";
-//     btnCorteFoto.style.display = "none";
+  arquivoDaFoto.addEventListener("change", (e) => {
+    // Mostra o botao de download
+    btnBaixar.style.display = "none";
+    btnCorteFoto.style.display = "none";
 
-//     //Criar um novo FileReader()
-//     const reader = new FileReader();
-//     const file = arquivoDaFoto.files.item(0);
-//     nomeFoto = file.name;
-//     //Usar a funcionalidade .readAsDataURL() para a leitura do arquivo
-//     reader.readAsDataURL(file);
+    //Criar um novo FileReader()
+    const reader = new FileReader();
+    const file = arquivoDaFoto.files.item(0);
+    nomeFoto = file.name;
+    //Usar a funcionalidade .readAsDataURL() para a leitura do arquivo
+    reader.readAsDataURL(file);
 
-//     //Adicionar ao src do elemento #imagem
-//     reader.onload = function (event) {
-//       imagem = new Image();
-//       imagem.src = event.target.result;
-//       imagem.onload = carregueFoto;
-//     };
+    //Adicionar ao src do elemento #imagem
+    reader.onload = function (event) {
+      imagem = new Image();
+      imagem.src = event.target.result;
+      imagem.onload = carregueFoto;
+    };
 
-//     //TODO: SELEÇÃO DA FERRAMENTA
-//     var startX,
-//       startY,
-//       relativeStartX,
-//       relativeStartY,
-//       endX,
-//       endY,
-//       relativeEndX,
-//       relativeEndY;
+    //TODO: SELEÇÃO DA FERRAMENTA
+    var startX,
+      startY,
+      relativeStartX,
+      relativeStartY,
+      endX,
+      endY,
+      relativeEndX,
+      relativeEndY;
 
-//     let inicioSelecao = false;
-//     let seletorFerramenta = document.getElementById("seletorFerramenta");
+    let inicioSelecao = false;
+    let seletorFerramenta = document.getElementById("seletorFerramenta");
 
-//     //Criar eventos do mouse
-//     const events = {
-//       mouseover() {
-//         //transforme cursor do mause em cruz
-//         this.style.cursor = "crosshair";
-//       },
-//       //Registrar posicionamentos iniciais de x e y(absolutos e relativos)
-//       mousedown() {
-//         const { clientX, clientY, offsetX, offsetY } = event;
-//         startX = clientX;
-//         startY = clientY;
-//         relativeStartX = offsetX;
-//         relativeStartY = offsetY;
-//         inicioSelecao = true;
-//       },
-//       //Registrar posicionamentos finais de X e Y e atualizar estilos visuais da seleção
-//       mousemove() {
-//         if (inicioSelecao) {
-//           endX = event.clientX;
-//           endY = event.clientY;
+    //Criar eventos do mouse
+    const events = {
+      mouseover() {
+        //transforme cursor do mause em cruz
+        this.style.cursor = "crosshair";
+      },
+      //Registrar posicionamentos iniciais de x e y(absolutos e relativos)
+      mousedown() {
+        const { clientX, clientY, offsetX, offsetY } = event;
+        startX = clientX;
+        startY = clientY;
+        relativeStartX = offsetX;
+        relativeStartY = offsetY;
+        inicioSelecao = true;
+      },
+      //Registrar posicionamentos finais de X e Y e atualizar estilos visuais da seleção
+      mousemove() {
+        if (inicioSelecao) {
+          endX = event.clientX;
+          endY = event.clientY;
 
-//           seletorFerramenta.style.display = "initial";
-//           seletorFerramenta.style.top = startY + "px";
-//           seletorFerramenta.style.left = startX + "px";
+          seletorFerramenta.style.display = "initial";
+          seletorFerramenta.style.top = startY + "px";
+          seletorFerramenta.style.left = startX + "px";
 
-//           seletorFerramenta.style.width = endX - startX + "px";
-//           seletorFerramenta.style.height = endY - startY + "px";
+          seletorFerramenta.style.width = endX - startX + "px";
+          seletorFerramenta.style.height = endY - startY + "px";
 
-//           //Resetar botao de corte
-//           btnCorteFoto.style.display = "none";
-//         }
-//       },
+          //Resetar botao de corte
+          btnCorteFoto.style.display = "none";
+        }
+      },
 
-//       //Registrar posicionamentos finais de X e Y (Relativo) e Remover flag de inicio de seleção
-//       mouseup() {
-//         inicioSelecao = false;
+      //Registrar posicionamentos finais de X e Y (Relativo) e Remover flag de inicio de seleção
+      mouseup() {
+        inicioSelecao = false;
 
-//         relativeEndX = event.layerX;
-//         relativeEndY = event.layerY;
+        relativeEndX = event.layerX;
+        relativeEndY = event.layerY;
 
-//         //mostrar botao de corte
-//         btnCorteFoto.style.display = "block";
-//       },
-//     };
+        //mostrar botao de corte
+        btnCorteFoto.style.display = "block";
+      },
+    };
 
-//     //Registrar eventos
-//     Object.keys(events).forEach((e) => {
-//       recebeFoto.addEventListener(e, events[e]);
-//     });
+    //Registrar eventos
+    Object.keys(events).forEach((e) => {
+      recebeFoto.addEventListener(e, events[e]);
+    });
 
-//     //TODO: HTML CANVAS API
+    //TODO: HTML CANVAS API
 
-//     //Criar elemento canvas
-//     let canvas = document.createElement("canvas");
+    //Criar elemento canvas
+    let canvas = document.createElement("canvas");
 
-//     //criar um contexto do elemnto
-//     let ctx = canvas.getContext("2d");
+    //criar um contexto do elemnto
+    let ctx = canvas.getContext("2d");
 
-//     //Atualizar o preview de imagem, agora, com o conteúdo do canvas ao inves da imagem
-//     function carregueFoto() {
-//       const { width, height } = imagem;
-//       canvas.width = width;
-//       canvas.height = height;
+    //Atualizar o preview de imagem, agora, com o conteúdo do canvas ao inves da imagem
+    function carregueFoto() {
+      const { width, height } = imagem;
+      canvas.width = width;
+      canvas.height = height;
 
-//       ctx.clearRect(0, 0, width, height);
-//       ctx.drawImage(imagem, 0, 0);
+      ctx.clearRect(0, 0, width, height);
+      ctx.drawImage(imagem, 0, 0);
 
-//       recebeFoto.src = canvas.toDataURL();
-//     }
+      recebeFoto.src = canvas.toDataURL();
+    }
 
-//     //TODO: CORTE IMAGEM
-//     // Adicionar evento click para quando clicar no botao
-//     btnCorteFoto.onclick = () => {
-//       // Calcular o corte proporcional ao tamanho da imagem na tela
-//       const { width: imgW, height: imgH } = imagem;
-//       const { width: rcbW, height: rcbH } = recebeFoto;
+    //TODO: CORTE IMAGEM
+    // Adicionar evento click para quando clicar no botao
+    btnCorteFoto.onclick = () => {
+      // Calcular o corte proporcional ao tamanho da imagem na tela
+      const { width: imgW, height: imgH } = imagem;
+      const { width: rcbW, height: rcbH } = recebeFoto;
 
-//       //Divida a largura da foto pela largura do preview
-//       const [widthFator, heightFator] = [+(imgW / rcbW), +(imgH / rcbH)];
+      //Divida a largura da foto pela largura do preview
+      const [widthFator, heightFator] = [+(imgW / rcbW), +(imgH / rcbH)];
 
-//       //Multiplicar a largura da seleção pelo fator de largura e o mesmo para a altura
-//       const [selecaoWidth, selecaoHeight] = [
-//         +seletorFerramenta.style.width.replace("px", ""),
-//         +seletorFerramenta.style.height.replace("px", ""),
-//       ];
+      //Multiplicar a largura da seleção pelo fator de largura e o mesmo para a altura
+      const [selecaoWidth, selecaoHeight] = [
+        +seletorFerramenta.style.width.replace("px", ""),
+        +seletorFerramenta.style.height.replace("px", ""),
+      ];
 
-//       const [corteWidth, corteHeight] = [
-//         +(selecaoWidth * widthFator),
-//         +(selecaoHeight * heightFator),
-//       ];
+      const [corteWidth, corteHeight] = [
+        +(selecaoWidth * widthFator),
+        +(selecaoHeight * heightFator),
+      ];
 
-//       //Calcule e guarde a posição x e y verdadeiras, para utilizar no ctx
-//       const [atualX, atualY] = [
-//         +(relativeStartX * widthFator),
-//         +(relativeStartY * heightFator),
-//       ];
+      //Calcule e guarde a posição x e y verdadeiras, para utilizar no ctx
+      const [atualX, atualY] = [
+        +(relativeStartX * widthFator),
+        +(relativeStartY * heightFator),
+      ];
 
-//       // pegar o ctx a imagem cortada
-//       const imagemCortada = ctx.getImageData(
-//         atualX,
-//         atualY,
-//         corteWidth,
-//         corteHeight
-//       );
+      // pegar o ctx a imagem cortada
+      const imagemCortada = ctx.getImageData(
+        atualX,
+        atualY,
+        corteWidth,
+        corteHeight
+      );
 
-//       // limpar o ctx
-//       ctx.clearRect(0, 0, ctx.width, ctx.height);
+      // limpar o ctx
+      ctx.clearRect(0, 0, ctx.width, ctx.height);
 
-//       // ajuste de proporcoes
-//       imagem.width = canvas.width = corteWidth;
-//       imagem.height = canvas.height = corteHeight;
+      // ajuste de proporcoes
+      imagem.width = canvas.width = corteWidth;
+      imagem.height = canvas.height = corteHeight;
 
-//       // adicionar a imagem cortada ao ctx
-//       ctx.putImageData(imagemCortada, 0, 0);
+      // adicionar a imagem cortada ao ctx
+      ctx.putImageData(imagemCortada, 0, 0);
 
-//       // esconder ferramenta de corte
-//       seletorFerramenta.style.display = "none";
+      // esconder ferramenta de corte
+      seletorFerramenta.style.display = "none";
 
-//       //Atualizar o preview da imagem
-//       recebeFoto.src = canvas.toDataURL();
+      //Atualizar o preview da imagem
+      recebeFoto.src = canvas.toDataURL();
 
-//       // Mostra o botao de download
-//       btnBaixar.style.display = "block";
-//     };
-//     // TODO: DOWNLOAD
-//     btnBaixar.onclick = () => {
-//       const a = document.createElement("a");
+      // Mostra o botao de download
+      btnBaixar.style.display = "block";
+    };
+    // TODO: DOWNLOAD
+    btnBaixar.onclick = () => {
+      const a = document.createElement("a");
 
-//       // Nome do arquivo de imagem
-//       a.download = "~cropped-image.png";
-//       a.href = canvas.toDataURL();
-//       a.click();
-//     };
-//   });
-// });
+      // Nome do arquivo de imagem
+      a.download = "~cropped-image.png";
+      a.href = canvas.toDataURL();
+      a.click();
+    };
+  });
+});
